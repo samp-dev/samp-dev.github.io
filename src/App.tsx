7b770224395e568020e178fe691e5e2b1074bdea
pkfln <peekfalone@outlook.com>
2019-04-01 22:15:36 +0200

    Added non-affiliation disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,29 @@ class App extends Component {
     return (
       <Row center={'xs'} between={'xs'} className={'center-v'}>
         <Col xs={12}>
-        <Row>
-          <Col xs>
-            <img src={splashScreen} alt={'SA-MP Development'} />
-          </Col>
-        </Row>
-        <Row around={'xs'}>
-          <Col xs={6} className={'link-icons around-xs row'}>
-            <a href={'https://github.com/samp-dev'}>
-              <img src={githubIcon} />
-            </a>
-            <a href={'https://discord.sa-mp.dev'}>
-              <img src={discordIcon} />
-            </a>
-            <a href={'https://fiddle.sa-mp.dev'}>
-              <img src={pawnIcon} />
-            </a>
-          </Col>
-        </Row>
+          <Row>
+            <Col xs>
+              <img src={splashScreen} alt={'SA-MP Development'} />
+            </Col>
+          </Row>
+          <Row around={'xs'}>
+            <Col xs={6} className={'link-icons around-xs row'}>
+              <a href={'https://github.com/samp-dev'}>
+                <img src={githubIcon} />
+              </a>
+              <a href={'https://discord.sa-mp.dev'}>
+                <img src={discordIcon} />
+              </a>
+              <a href={'https://fiddle.sa-mp.dev'}>
+                <img src={pawnIcon} />
+              </a>
+            </Col>
+          </Row>
+          <Row className={'footer'}>
+            <Col xs={12} className={'center-v'}>
+              This website and all sub-services are not endorsed or associated with sa-mp.com.
+            </Col>
+          </Row>
         </Col>
       </Row>
     );
